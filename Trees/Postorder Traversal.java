32514cfe3cee0c0b158c8debe054d113c8424fc6
Pradip222 <pvp9765@gmail.com>
2020-06-28 18:59:07 +0530

    Done with arrays!

---
+++ Trees/Postorder Traversal.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+    public static void postOrder(Node root) {
+        if(root.left==null && root.right==null){
+            System.out.print(root.data+" ");
+            return ;
+
+        }
+        else if(root.left==null){
+            postOrder(root.right);
+            System.out.print(root.data+" ");
+        }
+        else if(root.right==null){
+            postOrder(root.left);
+            System.out.print(root.data+" ");
+        }
+        else{
+             postOrder(root.left);
+             postOrder(root.right);
+        System.out.print(root.data+" ");
+        }
+
+    }
+
+	public static Node insert(Node root, int data) {
